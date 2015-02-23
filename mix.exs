2447ba748269b7f919d622d84076dd2bdf947dde
william de melo gueiros <williammelo@gmail.com>
2015-02-23 13:48:55 -0300

    ajuste de files e link no hex

--- mix.exs
+++ mix.exs
@@ -5,25 +5,15 @@ defmodule Brcpfcnpj.Mixfile do
     [app: :brcpfcnpj,
      version: "0.0.2",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
@@ -37,6 +27,8 @@ defmodule Brcpfcnpj.Mixfile do
     end
 	
   defp package do
-     [links: %{"GitHub" => "https://github.com/williamgueiros/Brcpfcnpj"]
+     [files: ~w(lib test config mix.exs README*),
+	 links: %{"GitHub" => "https://github.com/williamgueiros/Brcpfcnpj"}]
   end	
+
 end
