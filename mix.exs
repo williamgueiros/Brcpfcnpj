704d50bf1fc4f53c042b062ee8a9718bdb625c69
william de melo gueiros <williammelo@gmail.com>
2015-02-27 19:22:42 -0300

    incrementado a versao

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Brcpfcnpj.Mixfile do
 
   def project do
     [app: :brcpfcnpj,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      description: description,
      package: package,
