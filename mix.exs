f7897140130cb546598f68a54c54c3c045deac8a
william de melo gueiros <williammelo@gmail.com>
2015-03-02 14:04:34 -0300

    add change version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Brcpfcnpj.Mixfile do
 
   def project do
     [app: :brcpfcnpj,
-     version: "0.0.6",
+     version: "0.0.7",
      elixir: "~> 1.0",
      description: description,
      package: package,
