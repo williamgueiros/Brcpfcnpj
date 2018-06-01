bd53a426e3903c95662ee7687e745b3720b3f727
João Thallis <joaothallis.developer@gmail.com>
2018-06-01 00:34:28 -0300

    Format files

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Brcpfcnpj.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :brcpfcnpj,
-     version: "0.1.1",
-     elixir: "~> 1.2",
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :brcpfcnpj,
+      version: "0.1.1",
+      elixir: "~> 1.2",
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -15,9 +17,11 @@ defmodule Brcpfcnpj.Mixfile do
   end
 
   defp deps do
-    [{:earmark, "~> 0.2.1", only: :dev},
-     {:ex_doc, "~> 0.11.4", only: :dev},
-     {:inch_ex, "~> 0.5.1", only: :docs}]
+    [
+      {:earmark, "~> 0.2.1", only: :dev},
+      {:ex_doc, "~> 0.11.4", only: :dev},
+      {:inch_ex, "~> 0.5.1", only: :docs}
+    ]
   end
 
   defp description do
@@ -29,9 +33,11 @@ defmodule Brcpfcnpj.Mixfile do
   end
 
   defp package do
-    [files: ~w(lib test config mix.exs README*),
-     maintainers: ["William Gueiros"],
-     licenses: ["Unlicense"],
-     links: %{"GitHub" => "https://github.com/williamgueiros/Brcpfcnpj"}]
+    [
+      files: ~w(lib test config mix.exs README*),
+      maintainers: ["William Gueiros"],
+      licenses: ["Unlicense"],
+      links: %{"GitHub" => "https://github.com/williamgueiros/Brcpfcnpj"}
+    ]
   end
 end
