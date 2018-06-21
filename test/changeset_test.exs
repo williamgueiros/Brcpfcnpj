f494e4825c011a3ab2b41d1ac5acb531cdc545ab
Prem Prakash <prakash2ji@gmail.com>
2018-06-21 15:09:31 -0300

    mix format all files

--- test/changeset_test.exs
+++ test/changeset_test.exs
@@ -1,5 +1,5 @@
 defmodule ChangesetTest do
-	use ExUnit.Case
+  use ExUnit.Case
   import Brcpfcnpj.Changeset
 
   defp cast(changes) do
@@ -10,38 +10,38 @@ defmodule ChangesetTest do
     }
   end
 
-	test "changeset with invalid cnpj" do
-		changeset = cast(%{cnpj: "1234"}) |> validate_cnpj(:cnpj)
+  test "changeset with invalid cnpj" do
+    changeset = cast(%{cnpj: "1234"}) |> validate_cnpj(:cnpj)
     refute changeset.valid?
     %{errors: errors} = changeset
     assert errors[:cnpj] == "Invalid Cnpj"
-	end
+  end
 
-	test "changeset with valid cnpj" do
-		changeset = cast(%{cnpj: Brcpfcnpj.cnpj_generate}) |> validate_cnpj(:cnpj)
+  test "changeset with valid cnpj" do
+    changeset = cast(%{cnpj: Brcpfcnpj.cnpj_generate()}) |> validate_cnpj(:cnpj)
     assert changeset.valid?
-	end
+  end
 
-	test "changeset with invalid cpf" do
-		changeset = cast(%{cpf: "1234"}) |> validate_cpf(:cpf)
+  test "changeset with invalid cpf" do
+    changeset = cast(%{cpf: "1234"}) |> validate_cpf(:cpf)
     refute changeset.valid?
     %{errors: errors} = changeset
     assert errors[:cpf] == "Invalid Cpf"
-	end
+  end
 
-	test "changeset with valid cpf" do
-		changeset = cast(%{cpf: Brcpfcnpj.cpf_generate}) |> validate_cpf(:cpf)
+  test "changeset with valid cpf" do
+    changeset = cast(%{cpf: Brcpfcnpj.cpf_generate()}) |> validate_cpf(:cpf)
     assert changeset.valid?
-	end
+  end
 
-	test "custon error message" do
-		changeset = cast(%{cnpj: "1234", cpf: "123"})
-			|> validate_cnpj(:cnpj, message: "Cnpj Inválido")
-			|> validate_cpf(:cpf, message: "Cpf Inválido")
+  test "custon error message" do
+    changeset =
+      cast(%{cnpj: "1234", cpf: "123"})
+      |> validate_cnpj(:cnpj, message: "Cnpj Inválido")
+      |> validate_cpf(:cpf, message: "Cpf Inválido")
 
-		%{errors: errors} = changeset
-		assert errors[:cnpj] == "Cnpj Inválido"
-		assert errors[:cpf] == "Cpf Inválido"
-	end
-
+    %{errors: errors} = changeset
+    assert errors[:cnpj] == "Cnpj Inválido"
+    assert errors[:cpf] == "Cpf Inválido"
+  end
 end
--- test/cnpj_test.exs
+++ test/cnpj_test.exs
@@ -1,31 +1,32 @@
 defmodule CnpjTest do
-	use ExUnit.Case
-	
-	test "should be invalid with invalid number" do
-		cnpjs = ~w{69103604020160 00000000000000 69.103.604/0001-61 01618211000264 11111111111111}
-		Enum.each(cnpjs, fn(cnpj) ->
-			 assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) ==false
-		end
-		)
-	end
-	  
-	test "should be invalid with a number longer than 14 chars, even if the first 14 represent a valid number" do
-		cnpjs = ~w{691036040001-601 69103604000160a 69103604000160ABC 6910360400016000}
-		Enum.each(cnpjs, fn(cnpj) ->
-			 assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) ==false
-		end
-		)
-	end
-	
-	test "should be valid with correct number" do
-		cnpjs = ~w{69103604000160 69.103.604/0001-60 01518211/000264 01.5182110002-64 00.000.000/1447-89}
-		Enum.each(cnpjs, fn(cnpj) ->
-			 assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) ==true
-		end
-		)
-	end
+  use ExUnit.Case
 
-	test "should be invalid with nil input" do
-		assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: nil}) == false
-	end
+  test "should be invalid with invalid number" do
+    cnpjs = ~w{69103604020160 00000000000000 69.103.604/0001-61 01618211000264 11111111111111}
+
+    Enum.each(cnpjs, fn cnpj ->
+      assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) == false
+    end)
+  end
+
+  test "should be invalid with a number longer than 14 chars, even if the first 14 represent a valid number" do
+    cnpjs = ~w{691036040001-601 69103604000160a 69103604000160ABC 6910360400016000}
+
+    Enum.each(cnpjs, fn cnpj ->
+      assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) == false
+    end)
+  end
+
+  test "should be valid with correct number" do
+    cnpjs =
+      ~w{69103604000160 69.103.604/0001-60 01518211/000264 01.5182110002-64 00.000.000/1447-89}
+
+    Enum.each(cnpjs, fn cnpj ->
+      assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: cnpj}) == true
+    end)
+  end
+
+  test "should be invalid with nil input" do
+    assert Brcpfcnpj.cnpj_valid?(%Cnpj{number: nil}) == false
+  end
 end
