7f13e75fc185f47dd835ece88444030997abc52c
VitorTrin <emailreservadovitor@gmail.com>
2020-04-13 11:24:09 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Brcpfcnpj.Mixfile do
   def project do
     [
       app: :brcpfcnpj,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
