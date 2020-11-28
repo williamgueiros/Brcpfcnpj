e090fad6a929cfd5b164de1fbc0a5fa556e9dfa2
Alexandre de Souza <alexandre@aledsz.com.br>
2020-11-28 17:19:28 +0000

    Add database test support for Postgres

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,6 @@
+{:ok, _} = Application.ensure_all_started(:ecto)
+{:ok, _} = Application.ensure_all_started(:ecto_sql)
+{:ok, _} = Application.ensure_all_started(:postgrex)
+
+Code.require_file("support/test_repo.exs", __DIR__)
 ExUnit.start()
