34128e4cc641b30a8322811f6ddf7d57d9d0bd9c
Vítor Luís dos Santos Trindade <emailreservadovitor@gmail.com>
2021-12-14 11:09:11 -0300

    Bump deps and elixir (#38)

--- lib/cpf.ex
+++ lib/cpf.ex
@@ -4,5 +4,7 @@ defmodule Cpf do
   Os campos são:
     * `number` - Número do cpf ex: 11144477735, 111.444.777-35 ou 111.444.77735
   """
+  @type t :: %__MODULE__{}
+
   defstruct number: nil, tp_data: :cpf
 end
